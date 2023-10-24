5995abb95219d52168b1ec569618d8d5621a2915
idah-github <guantaiidah@gmaail.com>
2023-10-24 02:42:32 -0700

    debug

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
-i = 0
+""" i = 0
 for c in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(c - i)), end="")
-    i = 32 if i == 0 else 0
+    i = 32 if i == 0 else 0 """
+for t in range(122, 96, -1):
+    if t % 2 != 0:
+        print("{}".format(chr(t - 32)), end="")
+    else:
+        print("{}".format(chr(t)), end="")
