8e825af22c441925c6ad3a226002bd0d38d6dceb
idah-github <guantaiidah@gmaail.com>
2023-11-16 01:52:18 -0800

    debug

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,16 +1,22 @@
- a Rectangle subclass Square."""
+#!/usr/bin/python3
+"""inheritance"""
+
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square."""
+    """square class"""
 
     def __init__(self, size):
-        """Initialize a new square.
-
-        Args:
-            size (int): The size of the new square.
+        """initalizer
+        Args
+           size: size of side of square
         """
-        self.integer_validator("size", size)
-        super().__init__(size, size)
+        super().integer_validator('size', size)
+        Rectangle.__init__(self, size, size)
         self.__size = size
+
+    def __str__(self):
+        """returns dimension of square"""
+        return ('[Square] {}/{}'.format(self.__size, self.__size))
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""inheritance"""
-
-
+"""Defines a class Rectangle that inherits from BaseGeometry."""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle class"""
+    """Represent a rectangle using BaseGeometry."""
 
     def __init__(self, width, height):
-        """initializer
-        Args
-           width
-           height
+        """Intialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
         """
-        if super().integer_validator('width', width):
-            self.__width = width
-        if super().integer_validator('height', height):
-            self.__height = height
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
