8ebebd4493ced7fe7eb97f72af9ffbcc909e9a41
idah-github <guantaiidah@gmaail.com>
2023-11-22 02:29:07 -0800

    debug

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python3
-"""script"""
-
-
-import json
+"""Add all arguments to a Python list and save them to a file."""
 import sys
 
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-"""laods adds and saves a file"""
-
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-
-num = len(sys.argv)
-
-
-try:
-    data = load_from_json_file('add_item.json')
-except FileNotFoundError:
-    data = []
-
-for i in range(1, num):
-    data.append(sys.argv[i])
-save_to_json_file(data, 'add_item.json')
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
