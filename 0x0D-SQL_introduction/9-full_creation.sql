f1b8a2bf2827a17e03f8c4cc92380a1d378f0ffc
idah-github <guantaiidah@gmaail.com>
2023-12-13 09:04:52 -0800

    subquery

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,10 +1,10 @@
 -- create tbl with records
 
 CREATE TABLE IF NOT EXISTS second_table (
-	id INT,
-	name VARCHAR(256),
-	score INT);
-	INSERT INTO second_table (
+	`id` INT,
+	`name` VARCHAR(256),
+	`score` INT);
+	INSERT INTO second_table (`id`, `name`, `score`)
 		VALUES
 		(1, 'JOHN', 10),
 		(2, 'ALEX', 3),
