5810a54de3ce51ee1361b06e2c2ff05b41b89397
idah-github <guantaiidah@gmaail.com>
2024-01-11 06:04:55 -0800

    debug

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-/**
- * printing all 3 languages
- */
-const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-console.log(languages[0]);
-console.log(languages[1]);
-console.log(languages[2]);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
