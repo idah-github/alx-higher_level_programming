4067b0e085e4a9c71d2a1a570bd010ba1ebb7a57
idah-github <guantaiidah@gmaail.com>
2024-01-22 23:57:42 -0800

    debug

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,16 +2,19 @@
 /*
 creates sorted dictionary or lists of occurences
 */
-const dict = require('./101-data.js').dict;
+const dict = require('./101-data').dict;
 
+const totalist = Object.entries(dict);
+const vals = Object.values(dict);
+const valsUniq = [...new Set(vals)];
 const newDict = {};
-
-for (const key in dict) {
-  newDict[dict[key]] = [];
+for (const j in valsUniq) {
+  const list = [];
+  for (const k in totalist) {
+    if (totalist[k][1] === valsUniq[j]) {
+      list.unshift(totalist[k][0]);
+    }
+  }
+  newDict[valsUniq[j]] = list;
 }
-
-for (const key in dict) {
-  newDict[dict[key]].push(key);
-}
-
 console.log(newDict);
