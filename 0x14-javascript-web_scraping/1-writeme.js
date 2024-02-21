e8be3c78863d7f17696f4be1e56395b691e349de
idah-github <guantaiidah@gmail.com>
2024-02-21 08:02:27 +0000

    debug

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,13 +1,8 @@
-t fs = require('fs');
-
+#!/usr/bin/node
+const fs = require('fs');
 const filepath = process.argv[2];
-
-// The second argument is the string to write
 const string = process.argv[3];
-
 // write to file
 fs.writeFile(filepath, string, 'utf8', err=> {
-  if (err)
-    console.error(err);
+  if (err) console.error(err);
 )};
-  
